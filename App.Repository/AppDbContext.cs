c219c4a94ab4f62b5b9d4ae06aab87a983e0ce43
sait han uzun <saithan.uzun@gmail.com>
2023-01-17 21:25:08 +0000

    Add AppDbContext.cs

---
+++ App.Repository/AppDbContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using App.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace App.Repository
+{
+	public class AppDbContext : DbContext
+	{
+		public AppDbContext(DbContextOptions<AppDbContext> options):base(options)
+		{
+
+		}
+
+		public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductFeature> ProductFeatures { get; set; }
+
+
+
+    }
+}
+
