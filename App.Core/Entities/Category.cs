e6c04e645f281d24315be4e2b05bf5ab580b71c5
sait han uzun <saithan.uzun@gmail.com>
2023-01-17 00:22:37 +0000

    Add Entities

---
+++ App.Core/Entities/Category.cs
@@ -0,0 +1,9 @@
+namespace App.Core.Entities
+{
+    public class Category : BaseEntity
+    {
+        public string Name { get; set; }
+
+        public ICollection<Product> Products { get; set; }
+    }
+}
