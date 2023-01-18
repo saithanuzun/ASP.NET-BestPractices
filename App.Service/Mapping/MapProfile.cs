8cc1a539b7decc1e268e26d04c36448eb32166f0
saithanuzun <saithan.uzun@gmail.com>
2023-01-18 16:20:06 +0000

    Add AutoMapper

---
+++ App.Service/Mapping/MapProfile.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using App.Core.DTOs;
+using App.Core.Entities;
+using AutoMapper;
+
+namespace App.Service.Mapping
+{
+    public class MapProfile : Profile
+    {
+        public MapProfile()
+        {
+            CreateMap<Product,ProductDto>().ReverseMap();
+            CreateMap<Category,CategoryDto>().ReverseMap();
+            CreateMap<ProductFeature,ProductFeatureDto>().ReverseMap();
+            CreateMap<ProductUpdateDto,Product>();
+
+        }
+    }
+}
